ccf401cccb9148833b94e45025cc5638ad716f1f
PointlessUsername <42281247+PointlessUsername@users.noreply.github.com>
2022-10-17 12:40:34 -0600

    Fixed Gradle

--- app/src/main/java/com/example/mealy/MainActivity.java
+++ app/src/main/java/com/example/mealy/MainActivity.java
@@ -16,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
