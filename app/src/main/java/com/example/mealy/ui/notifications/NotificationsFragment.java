b4e1558a71d7f218598f7407f96d16f8b540104b
5yph <lxwang@ualberta.ca>
2022-10-25 12:16:28 -0600

    placing recipe list in notifications fragment

--- app/src/main/java/com/example/mealy/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/mealy/ui/notifications/NotificationsFragment.java
@@ -4,17 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.mealy.R;
 import com.example.mealy.databinding.FragmentNotificationsBinding;
 
+// This is where I will temporarily put my RecipeList view
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
+
+    private Spinner sortSpinner;
+    private ListView recipeList;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,6 +30,9 @@ public class NotificationsFragment extends Fragment {
 
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        sortSpinner = root.findViewById(R.id.recipesort);
+        recipeList = root.findViewById(R.id.recipestorage);
 
         final TextView textView = binding.textNotifications;
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
