49ae11466fa67628c5cf0864de484dbf30d2f24b
Lawrence <lxwang@ualberta.ca>
2022-11-03 23:41:58 -0600

    added test cases for recipe comparison

--- app/src/androidTest/java/com/example/mealy/RecipeComparatorTest.java
+++ app/src/androidTest/java/com/example/mealy/RecipeComparatorTest.java
@@ -12,57 +12,139 @@ import java.util.List;
 
 public class RecipeComparatorTest {
 
+    public List<Ingredient> makeIngredientList() {
+        // add sample ingredient for recipe list constructor (not being compared)
+        Ingredient apple = new Ingredient("Apple",
+                "Red",
+                "0",
+                "lb",
+                "Weight",
+                "Raw Food",
+                "Pantry",
+                "2022-12-05");
+
+        // make ingredient list
+        List ingredientList = new ArrayList<Ingredient>();
+        ingredientList.add(apple);
+
+        return ingredientList;
+    }
+
+    // make some recipes
+    Recipe applePie = new Recipe("Apple pie",
+            "Delicious apple pie made from real apple",
+            4, 1, 30,
+            "Baked", makeIngredientList());
+
+    Recipe friedApple = new Recipe("Fried apple",
+            "Apples that are fried in a vat of oil",
+            2, 0, 15,
+            "Fried", makeIngredientList());
+
+    Recipe candyApple = new Recipe("Candy apple",
+            "Whole apples covered in a sugar coating",
+            1, 0, 45,
+            "Dessert", makeIngredientList());
+
+
+    // make recipe list
     public ArrayList<Recipe> makeRecipeList() {
         ArrayList<Recipe> recipeArrayList = new ArrayList<>();
 
-        // add sample ingredient for recipe list (change later)
-        Ingredient rat_hair = new Ingredient("rat hair",
-                "hair from a rat",
-                "100",
-                "strands",
-                "protein",
-                "rodent",
-                "pantry",
-                "2025-12-13");
-
-        List ingredientList = new ArrayList<Ingredient>();
-        ingredientList.add(rat_hair);
-
-        // add sample items
-        recipeArrayList.add(new Recipe("Meat Rat",
-                "Yummy for my tummy",
-                5, 3, 10,
-                "Grilled",
-                R.drawable.meat_rat,
-                ingredientList));
-
-        recipeArrayList.add(new Recipe("Meat Skull",
-                "Going to hell",
-                3, 4, 30,
-                "Fried",
-                R.drawable.meatskull,
-                ingredientList));
-
-        recipeArrayList.add(new Recipe("Rat Hair",
-                "Going to hell",
-                3, 4, 30,
-                "Fried",
-                R.drawable.rathair,
-                ingredientList));
+        recipeArrayList.add(applePie);
+        recipeArrayList.add(friedApple);
+        recipeArrayList.add(candyApple);
 
         return recipeArrayList;
     }
 
+    // test title comparator
     @Test
-    public void testCompare() {
-        int asc = 1;
-
+    public void testCompareTitle() {
         ArrayList<Recipe> recipeArrayList = makeRecipeList();
 
-        CompareRecipes compare = new CompareRecipes("selectedItem", asc);
+        // sorting should be alphabetical: apple pie -> candy apple -> fried apple
+        CompareRecipes compare = new CompareRecipes("Title", 1);
         Collections.sort(recipeArrayList, compare.returnComparator());
 
-        assertEquals(1+1, 2);
+        assertTrue(recipeArrayList.get(0) == applePie
+                && recipeArrayList.get(1) == candyApple
+                && recipeArrayList.get(2) == friedApple);
+
+        // sort in reverse alphabetical: fried apple -> candy apple -> apple pie
+        CompareRecipes compareReverse = new CompareRecipes("Title", -1);
+        Collections.sort(recipeArrayList, compareReverse.returnComparator());
+
+        assertTrue(recipeArrayList.get(0) == friedApple
+                && recipeArrayList.get(1) == candyApple
+                && recipeArrayList.get(2) == applePie);
+    }
+
+    // test prep time comparator
+    @Test
+    public void testComparePrepTime() {
+        ArrayList<Recipe> recipeArrayList = makeRecipeList();
+
+        // sorting should be from least to most prep time: fried apple -> candy apple -> apple pie
+        CompareRecipes compare = new CompareRecipes("Prep Time", 1);
+        Collections.sort(recipeArrayList, compare.returnComparator());
+
+        assertTrue(recipeArrayList.get(0) == friedApple
+                && recipeArrayList.get(1) == candyApple
+                && recipeArrayList.get(2) == applePie);
+
+        // sorting should be from most to least prep time: apple pie -> candy apple -> fried apple
+        CompareRecipes compareReverse = new CompareRecipes("Prep Time", -1);
+        Collections.sort(recipeArrayList, compareReverse.returnComparator());
+
+        assertTrue(recipeArrayList.get(0) == applePie
+                && recipeArrayList.get(1) == candyApple
+                && recipeArrayList.get(2) == friedApple);
+
+    }
+
+    // test category comparator
+    @Test
+    public void testCompareCategory() {
+        ArrayList<Recipe> recipeArrayList = makeRecipeList();
+
+        // sorting should be from alphabetical category: apple pie -> candy apple -> fried apple
+        CompareRecipes compare = new CompareRecipes("Category", 1);
+        Collections.sort(recipeArrayList, compare.returnComparator());
+
+        assertTrue(recipeArrayList.get(0) == applePie
+                && recipeArrayList.get(1) == candyApple
+                && recipeArrayList.get(2) == friedApple);
+
+        // sorting should be from reverse alphabetical category: fried apple -> candy apple -> apple pie
+        CompareRecipes compareReverse = new CompareRecipes("Category", -1);
+        Collections.sort(recipeArrayList, compareReverse.returnComparator());
+
+        assertTrue(recipeArrayList.get(0) == friedApple
+                && recipeArrayList.get(1) == candyApple
+                && recipeArrayList.get(2) == applePie);
+    }
+
+    // test servings comparator
+    @Test
+    public void testServings() {
+        ArrayList<Recipe> recipeArrayList = makeRecipeList();
+
+        // sorting should be from least to most servings: candy apple -> fried apple -> apple pie
+        CompareRecipes compare = new CompareRecipes("Servings", 1);
+        Collections.sort(recipeArrayList, compare.returnComparator());
+
+        assertTrue(recipeArrayList.get(0) == candyApple
+                && recipeArrayList.get(1) == friedApple
+                && recipeArrayList.get(2) == applePie);
+
+        // sorting should be from most to least: apple pie -> fried apple -> candy apple
+        CompareRecipes compareReverse = new CompareRecipes("Servings", -1);
+        Collections.sort(recipeArrayList, compareReverse.returnComparator());
+
+        assertTrue(recipeArrayList.get(0) == applePie
+                && recipeArrayList.get(1) == friedApple
+                && recipeArrayList.get(2) == candyApple);
     }
 
 }
