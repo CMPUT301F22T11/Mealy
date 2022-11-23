e93ee5f1f15d1c96aa6ea6cbabd48d5eb004783c
Kody Diep <kodydiep@gmail.com>
2022-11-23 16:34:41 -0700

    Commit

--- app/src/main/java/com/example/mealy/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/mealy/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Meal Plan");
     }
 
     public LiveData<String> getText() {
