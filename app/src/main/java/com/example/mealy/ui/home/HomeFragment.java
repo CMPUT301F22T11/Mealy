d167f866e578867b3bc3c93a0bc4bbedf7d9e5a4
Lawrence <lxwang@ualberta.ca>
2022-11-23 14:55:41 -0700

    added listview

--- app/src/main/java/com/example/mealy/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/mealy/ui/home/HomeFragment.java
@@ -4,17 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.mealy.R;
 import com.example.mealy.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+
+    private ListView mealPlanListView; // list of mealplans
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,6 +30,9 @@ public class HomeFragment extends Fragment {
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        mealPlanListView = root.findViewById(R.id.mealplanlistview);
+
         return root;
     }
 
