8ed9f1367c33c5d15876451c8070789628aa90bf
HiltonTR <66839605+HiltonTR@users.noreply.github.com>
2022-11-28 01:30:59 -0700

    Update ShoppingIngredient.java

--- app/src/main/java/com/example/mealy/ui/shoppingList/ShoppingIngredient.java
+++ app/src/main/java/com/example/mealy/ui/shoppingList/ShoppingIngredient.java
@@ -34,45 +34,81 @@ public class ShoppingIngredient {
     }
 
     /**
-     *
-     * @return
+     * Gets the name of the item
+     * @return the name of the item
      */
     public String getName() {
         return General.blankIfVoid(name);
     }
 
+    /**
+     * Gets the description of the item
+     * @return the description of the item
+     */
     public String getDescription() {
         return General.blankIfVoid(description);
     }
 
+    /**
+     * Gets the quantity of the item
+     * @return the quantity of the item
+     */
     public String getQuantity() {
         return General.blankIfVoid(quantity);
     }
 
+    /**
+     * Gets the unit of the item
+     * @return the unit of the item
+     */
     public String getUnit() {
         return General.blankIfVoid(unit);
     }
 
+    /**
+     * Gets the category of the item
+     * @return the category of the item
+     */
     public String getCategory() {
         return General.blankIfVoid(category);
     }
 
+    /**
+     * sets the name of the item
+     * @param name of the item
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * sets the description of the item
+     * @param description of the item
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * sets the quantity of the item
+     * @param amount of the item
+     */
     public void setQuantity(String amount) {
         this.quantity = amount;
     }
 
+    /**
+     * sets the unit of the item
+     * @param unit of the item
+     */
     public void setUnit(String unit) {
         this.unit = unit;
     }
 
+    /**
+     * sets the category of the item
+     * @param category of the item
+     */
     public void setCategory(String category) {
         this.category = category;
     }
